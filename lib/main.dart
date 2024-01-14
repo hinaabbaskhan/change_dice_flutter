6da881ff37682b0b94a5332dc9bfd7fb99ee4f31
Hina Khan <47082795+hinaabbaskhan@users.noreply.github.com>
2024-01-14 14:11:03 +0500

    roll dicee functionality completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -6,7 +7,7 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee'),
+          title: Text('Dice'),
           backgroundColor: Colors.red,
         ),
         body: DicePage(),
@@ -15,9 +16,63 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int firstDiceNumber = 1;
+  int secondDiceNumber = 5;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          // first dice widget
+          Expanded(
+            flex: 1,
+            child: Padding(
+              padding: const EdgeInsets.all(8),
+              child: TextButton(
+                onPressed: () {
+                  print('I am second dice');
+
+                  setState(() {
+                    firstDiceNumber = Random().nextInt(6) + 1; //(0----5)+1
+                    secondDiceNumber = Random().nextInt(6) + 1;
+                  });
+                },
+                child: Image(
+                  image: AssetImage('images/dice$firstDiceNumber.png'),
+                ),
+              ),
+            ),
+          ),
+
+          // second dice widget
+          Expanded(
+            flex: 1,
+            child: Padding(
+              padding: const EdgeInsets.all(8),
+              child: TextButton(
+                onPressed: () {
+                  print('I am second dice');
+
+                  setState(() {
+                    firstDiceNumber = Random().nextInt(6) + 1; //(0----5)+1
+                    secondDiceNumber = Random().nextInt(6) + 1;
+                  });
+                },
+                child: Image(
+                  image: AssetImage('images/dice$secondDiceNumber.png'),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
